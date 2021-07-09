9ad16f9829ad168a09e947f29b94cda9fa10e62b
JisuLim <sanola3@gmail.com>
2021-07-10 02:07:30 +0900

    crud

--- third/urls.py
+++ third/urls.py
@@ -5,4 +5,7 @@ from . import views
 
 urlpatterns = [
   path('list/', views.list, name='list'),
+  path('create/', views.create, name='restaurant-create'),
+  path('update/', views.update, name='restaurant-update'),
+  path('delete/', views.delete, name='restaurant-delete'),
 ]
